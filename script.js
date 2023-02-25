1d4a78729ff54aa1f55c77ea85646bb2cbaf1bd5
Jowmagas <jowmagas@gmail.com>
2023-02-25 13:44:51 -0300

    entrega realizada

--- script.js
+++ script.js
@@ -21,8 +21,32 @@ passed = false;
 // if (gender === "male")  
 // **sua lógica a partir daqui**
 
-
-
+if (gender === "male") {
+    if (height >= 1.70) {
+        if (barReps >= 6 || barSeconds <= 15) {
+            if (abs >= 41) {
+                if ((runDistance >= 3000 && runTime <= 720) || (runDistance >= 5000 && runTime <= 1200)) {
+                    if ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30) {
+                        passed = true;
+                    }
+                }
+            }
+        }
+    }
+}
+if (gender === "female") {
+    if (height >= 1.60) {
+        if (barReps >= 5 || barSeconds <= 12) {
+            if (abs >= 41) {
+                if ((runDistance >= 4000 && runTime <= 900) || (runDistance >= 6000 && runTime <= 1320)) {
+                    if ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30) {
+                        passed = true;
+                    }
+                }
+            }
+        }
+    }
+}
 
 // Deve conter apenas esse console.log no seu código.
 // Senão os testes não irão funcionar.
